c534e62bbfaddbcb7c44a050b1e2413ac7dc2b6d
thiemimi <thiemi.h.favaro@gmail.com>
2024-04-03 22:04:47 -0300

    alterações em resenha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,12 +12,12 @@ export default function Home() {
     },
     {
       id: 2,
-      nome: "Resenha",
+      nome: "Resenha2",
       baba: "sei la"
     },
     {
       id: 3,
-      nome: "Resenha",
+      nome: "Resenha3",
       baba: "sei la"
     }
 
@@ -26,13 +26,17 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center">
       <NavBar active="resenhas"/>
 
-      <section className="bg-green-400 rounded min-w-[1000px] p-4 m-4">
-        <h2 className="text-2xl font-semibold">Minhas Resenhas</h2>
+      <section className="rounded min-w-[1000px] p-4 m-4">
+      
+        <div className="flex p-2">
+          <h2 className="text-2xl font-semibold p-2">Minhas Resenhas</h2>
+          <button className="text-l bg-red-300 rounded p-2 m-1">Nova resenha</button>
+        </div>
         <div id="data">
           {resenhas.map(resenha => {
             return(
-              <div key={resenha.id} className="flex justify-between bg-green-800 rounded min-w-[150px] min-h-[100px] p-4 m-4" id="data-row">
-                <div className="flex gap-1">
+              <div key={resenha.id} className="inline-block justify-between bg-green-800 rounded min-w-[250px] min-h-[200px] p-4 m-4" id="data-row">
+                <div className="flex gap-1 justify-between">
                   <span>{resenha.nome}</span>
                   <ChevronDown/>
                 </div>
@@ -41,6 +45,7 @@ export default function Home() {
           })}
         </div>
       </section>
+      
     </main>
   );
 }
